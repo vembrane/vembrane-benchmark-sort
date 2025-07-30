6b81fa424bcb825157d2c3b401813df1e3e388c9
Johannes Koester <johannes.koester@uni-due.de>
2025-07-30 16:41:51 +0200

    fixes

--- workflow/scripts/subsample_variants.py
+++ workflow/scripts/subsample_variants.py
@@ -1,3 +1,6 @@
+import sys
+sys.stderr = open(snakemake.log[0], "w")
+
 import random
 from pysam import VariantFile
 
@@ -8,7 +11,7 @@ with VariantFile(snakemake.input[0], "r") as vcf_in:
     records = list(vcf_in)
     random.shuffle(records)
 
-    for fraction, vcf_out in zip(snakemake.params.fractions, snakemake.output):
+    for fraction, vcf_out in zip(snakemake.params.subsample_fractions, snakemake.output):
         with VariantFile(vcf_out, "wb", header=vcf_in.header) as vcf_out:
             for record in records[:round(len(records) * fraction)]:
                 vcf_out.write(record)
